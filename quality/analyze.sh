f951a1cc4e0019cba8b03b903e232785eb4628a3
Laurent Minguet <laurent@spareka.com>
2021-04-29 13:37:00 +0200

    update all the packages + go on php 7.4

--- quality/analyze.sh
+++ quality/analyze.sh
@@ -9,12 +9,12 @@ rm -rf $LOG_FOLDER
 mkdir -p $LOG_FOLDER
 
 ./vendor/bin/phpqa \
-    --analyzedDirs src \
-    --ignoredDirs vendor,Tests \
-    --tools phpmetrics,phploc,pdepend,phpcs:0,phpmd:0,phpcpd:0,parallel-lint:0 \
-    --config ./ \
-    --buildDir $LOG_FOLDER \
-    --report offline \
-    --execution no-parallel
+    --analyzedDirs "src" \
+    --ignoredDirs "vendor" \
+    --tools "phpmetrics,phploc,pdepend,phpcs:0,phpmd:0,phpcpd:0,parallel-lint:0" \
+    --config "./" \
+    --buildDir "$LOG_FOLDER" \
+    --report "offline" \
+    --execution "no-parallel"
 
 firefox "${LOG_FOLDER}phpqa.html"
