81b2a2b50da674179cf79d4a691f44e2c178245b
Laurent Minguet <laurent@spareka.com>
2026-04-01 17:29:10 +0200

    better code quality

--- website/src/App/Controller/MainController.php
+++ website/src/App/Controller/MainController.php
@@ -15,7 +15,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/')]
 class MainController extends AbstractController
